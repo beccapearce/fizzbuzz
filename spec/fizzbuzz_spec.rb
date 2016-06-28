bb3e950f69c6313c22531faeb7dd63ab60e0b584
Rebecca Pearce <becciepearce@hotmail.co.uk>
2016-06-28 13:43:05 +0100

    Conditions added for fizz (%3) buzz (%5) and fizzbuzz (%3&5) and condition to elimate strings

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,25 @@
 require "fizzbuzz"
 
 describe "fizzbuzz" do
-  it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq "fizz"
+   it 'returns "fizz" when passed 3' do
+     expect(fizzbuzz(3)).to eq "fizz"
+  end
+  it 'returns 4 when passed 4' do
+    expect(fizzbuzz(4)).to eq 4
+  end
+  it 'returns "fizz" when passed 6' do
+    expect(fizzbuzz(6)).to eq "fizz"
+  end
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq "buzz"
+  end
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq "fizzbuzz"
+  end
+  it 'returns "fizzbuzz" when passed 30' do
+    expect(fizzbuzz(30)).to eq "fizzbuzz"
+  end
+  it 'returns "ERROR" when passed Vimto' do
+    expect(fizzbuzz("Vimto")).to eq "ERROR"
   end
 end
